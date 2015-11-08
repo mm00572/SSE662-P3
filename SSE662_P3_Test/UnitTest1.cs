2dd8749f4b47eb5989b3a6f838a46f2b791a0d7d
mm00572 <mm00572@georgiasouthern.edu>
2015-11-08 13:03:43 -0500

    Initial

---
+++ SSE662_P3_Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SSE662_P3_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
