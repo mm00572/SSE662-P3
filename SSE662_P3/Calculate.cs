0e7536e52740bc0d68f0cb0dacce91df2b3994ee
mm00572 <mm00572@georgiasouthern.edu>
2015-11-08 15:35:05 -0500

    Remove Assignment Parameters

---
+++ SSE662_P3/Calculate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSE662_P3
+{
+    public class Calculate
+    {
+        public static double Compute(string operand, double operator1, double operator2)
+        {
+            double result = 0.0;
+
+            switch(operand)
+            {
+                case "+":
+                    result = operator1 + operator2;
+                    break;
+                case "-":
+                    result = operator1 - operator2;
+                    break;
+                case "*":
+                    result = operator1 * operator2;
+                    break;
+                case "/":
+                    result = operator1 / operator2;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
