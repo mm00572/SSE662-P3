804d05c0fb10f30321bbdada1f98a607fb8b4020
mm00572 <mm00572@georgiasouthern.edu>
2015-11-08 19:03:51 -0500

    Encapsulate Collection

---
+++ SSE662_P3/Company.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSE662_P3
+{
+    public class Company
+    {
+        private readonly List<Employee> _employees;
+
+        public Company()
+        {
+            _employees = new List<Employee>();
+        }
+
+        public void AddEmployee(Employee e)
+        {
+            _employees.Add(e);
+        }
+
+        public void RemoveEmployee(Employee e)
+        {
+            _employees.Remove(e);
+        }
+
+        public int GetNumberOfEmployees()
+        {
+            return _employees.Count;
+        }
+    }
+}
---
+++ SSE662_P3/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSE662_P3
+{
+    public class Employee
+    {
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public Employee(string fName, string lName)
+        {
+            _firstName = fName;
+            _lastName = lName;
+        }
+        
+    }
+}
